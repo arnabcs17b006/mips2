d2f11188a53f22cdcb80e0cf3f9324baae453406
arnabcs17b006 <46973323+arnabcs17b006@users.noreply.github.com>
2019-03-29 15:20:51 +0530

    Update 4.asm

--- 4.asm
+++ 4.asm
@@ -1,18 +1,22 @@
 main:
-	addi $sp,$sp,-12
-	sw $s0,8($sp)
-	sw $s1,4($sp)
-	li $s0, 1
-	li $s1, 2
-	and $t0,$s0,$s1
-	or $t1,$s0,$s1
-	xor $t2, $s0.$s1
-	xnor $t3,$s0.$s1
-	xor $s0,$s0,$s0 		 #reset to zero
-	li $s0,1
-	nor $s1, $s1, $s1	 	#one's complement
-	addi $s1,$s1,1 			#two's complement
-	lw $s1,4($sp)
-	lw $s0,8($sp)
-	addi $sp,$sp,12
-	j $31
+	addi $sp,$sp,-24
+	sw   $s0,20($sp)
+	sw   $s1,16($sp)
+	sw   $s2,12($sp)
+	sw   $s3,8($sp)
+	sw   $s4,4($sp)
+	sw   $s5,0($sp)
+	addi $s0,$zero,2
+	addi $s1,$zero,3
+	or  $s2,$s0,$s1
+	and   $s3,$s0,$s1
+	xor  $s4,$s0,$zero
+	nor  $s5,$s0,$s1
+	lw   $s5,0($sp)
+	lw   $s4,4($sp)
+	lw   $s3,8($sp)
+	lw   $s2,12($sp)
+	lw   $s1,16($sp)
+	lw   $s0,20($sp)
+	addi $sp,$sp,24
+j $31

54461731fb36c4b4b580d0b7ba86718f2a0a73a3
arnabcs17b006 <46973323+arnabcs17b006@users.noreply.github.com>
2019-03-29 15:10:18 +0530

    Update 1_b.asm

--- 1_b.asm
+++ 1_b.asm
@@ -1,5 +1,5 @@
 main:
-addi $sp,$sp,-8
+  addi $sp,$sp,-8
   sw $s0,4($sp)
   sw $s1,0($sp)
   addi $s0,$zero,2
@@ -7,7 +7,7 @@ addi $sp,$sp,-8
   add $s0,$s0,$s1
   sub $s1,$s0,$s1
   sub $s0,$s0,$s1
-  lw $s1,0($sp)
-  lw $s0,4($sp)
+  lw $s0,0($sp)
+  lw $s1,4($sp)
   addi $sp,$sp,8
-  j $31
+j $31

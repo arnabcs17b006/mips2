2d9ac362d334555fd1af235742de84acd87a222c
arnabcs17b006 <46973323+arnabcs17b006@users.noreply.github.com>
2019-03-29 15:58:03 +0530

    Update 3_1.asm

--- 3_1.asm
+++ 3_1.asm
@@ -1,16 +1,13 @@
 main:
-	addi $sp,$sp,-16
-        sw $s0,12($sp)
-	sw $s1,8($sp)
-	sw $s2,4($sp)
-	sw $s3,0($sp)
-	addi $s0,$zero,5
-	sll $s1,$s0,1
-	sll $s2,$s0,2
-	sll $s3,$s0,3
-	lw $s3,0($sp)
-	lw $s2,4($sp)
-	lw $s1,8($sp)
-	lw $s0,12($sp)
-	addi $sp,$sp,16;
-j $31
+li $t0,1
+li $t1,2
+li $s0,4
+li $s1,8
+
+L:
+beq $s1,$t0,Exit
+sll $s0,$s0,1
+srl $s1,$s1,1
+j L
+
+Exit
